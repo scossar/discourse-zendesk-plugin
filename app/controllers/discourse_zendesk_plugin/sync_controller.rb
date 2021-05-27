86e62a462e8b7386995694cdb1c90d531d8998be
scossar <scossar3@gmail.com>
2021-05-27 16:39:09 -0700

    Quick test to see if Zendesk attachments can be synced to Discourse

--- app/controllers/discourse_zendesk_plugin/sync_controller.rb
+++ app/controllers/discourse_zendesk_plugin/sync_controller.rb
@@ -30,13 +30,42 @@ module DiscourseZendeskPlugin
         unless existing_comment.present?
           post = topic.posts.create!(
             user: user,
-            raw: latest_comment.body
+            raw: build_raw_post_body(comment)
           )
           update_post_custom_fields(post, latest_comment)
         end
       end
 
       render json: {}, status: 204
+    end
+
+    def build_raw_post_body(comment)
+      # Use body to preserve legacy
+      # return comment.body unless SiteSetting.zendesk_append_attachments?
+
+      # Prefer the html_body to preserve inline links if available possible
+      prefix = comment.html_body.presence || comment.body.presence || ''
+      prefix + build_attachments_body(comment)
+    end
+
+    def build_attachments_body(comment)
+      return '' if comment.attachments.blank?
+
+      "\n\n**Attachments**\n" + comment.attachments.map do |attachment|
+        break '' if attachment.deleted
+        thumbnail = attachment.thumbnails&.first
+        if thumbnail.present? && thumbnail.deleted === false
+          "[![](#{extract_content_url(thumbnail)})](#{extract_content_url(attachment)}) "
+        else
+          "\n* [#{attachment.file_name} (#{attachment.content_type})](#{extract_content_url(attachment)})"
+        end
+      end.sort.reverse.join('') # Put the thumbnails is a line above the links
+    rescue StandardError => e
+      ''
+    end
+
+    def extract_content_url(trackie)
+      trackie.mapped_content_url.presence || trackie.content_url
     end
 
     private
